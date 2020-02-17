0d76edd1b37e6a313fb85f02a6977c20df2efbfc
KLisabeth <professional.translations4ppl@gmail.com>
2020-02-17 12:24:01 +0100

    convert between F and C

--- challenges/temp-converter.js
+++ challenges/temp-converter.js
@@ -2,7 +2,19 @@ console.log('tempConverter script has loaded');
 
 // write this function!
 function tempConverter(temperature, degrees) {
+  if (degrees === 'farenheit') {
+    //	Multiply by 9, then divide by 5, then add 32
+    const temp = (temperature*9)/5+32;
+    return temp;
+  } else if (degrees === 'celcius') {
+    //Deduct 32, then multiply by 5, then divide by 9
+    const temp = ((temperature-32)*5)/9;
+    return temp;
+  } else {
+    return degrees+' is not supported';
+  }
 }
+//return(`${userTempStr}${userDegrees ==="farenheit" ? "celcius": "farenheit"} = ${result} ${userDegrees}`);
 
 // if the user wants to convert to farenheit
 console.assert(tempConverter(53, 'farenheit') === 127.4, 'first');
@@ -10,9 +22,9 @@ console.assert(tempConverter(0, 'farenheit') === 32, 'second');
 console.assert(tempConverter(-40, 'farenheit') === -40, 'third');
 
 // if the user wants to convert to celcius
-console.assert(tempConverter(0, 'celcius') === 32, 'fourth');
-console.assert(tempConverter(4, 'celcius') === 39.2, 'fifth');
-console.assert(tempConverter(12.4, 'celcius') === 54.32, 'sixth');
+console.assert(tempConverter(127.4, 'celcius') === 53, 'fourth');
+console.assert(tempConverter(32, 'celcius') === 0, 'fifth');
+console.assert(tempConverter(-40, 'celcius') === -40, 'sixth');
 
 // if the user inputs an invalid degree
 console.assert(tempConverter(34, 'toad') === 'toad is not supported', 'seventh');
@@ -24,16 +36,15 @@ function tempConverterHandler() {
   // prompt the user for a noun, verb and adjective
   const userTempStr = prompt('enter a temperature to convert');
   // cast userDegreesStr to a Number, and assign the value to userDecrees
+  let userTemp = Number(userTempStr);
   const userDegrees = prompt('would you like to convert to farenheit or celcius?');
-
-  console.assert(typeof userDegrees === 'number', "don't forget to cast userDegrees to a string!");
+  
+  //console.assert(typeof userTempStr === 'number', "don't forget to cast userDegrees to a string!");
 
   // perform core logic
-  // write this line!
-
+  let result = tempConverter(userTemp, userDegrees);// write this line!
   // alert result for the user
-  // write this line!
-
+  alert(result); // write this line!
   // log action for the developer
   console.log('\n--- tempConverter ---');
   console.log('userTemp:', '(' + typeof userTemp + '),', userTemp);
